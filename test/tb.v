27188ee93fe6c55f0a03fb39a08aee7753bc1fdb
ethansam9 <ethanhubtop@gmail.com>
2025-10-05 15:56:49 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_ethansam8_counter dut (
+  tt_um_ethansam9_counter dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
