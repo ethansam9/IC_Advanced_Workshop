e0cd442e921e8782eec4ec89980c1996605a2c5f
ethansam9 <ethanhubtop@gmail.com>
2025-10-05 15:45:18 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_oconnt_counter (
+module tt_um_ethansam9_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
